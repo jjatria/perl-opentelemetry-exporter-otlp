c4b20625d4a1a106c21e1b6723649a3e5926a605
José Joaquín Atria <j.atria@cv-library.co.uk>
2024-07-17 09:48:48 +0100

    Prepare for next release

--- lib/OpenTelemetry/Proto.pm
+++ lib/OpenTelemetry/Proto.pm
@@ -1,7 +1,7 @@
 package OpenTelemetry::Proto;
 # ABSTRACT: The protobuf definitions for the OpenTelemetry Protocol
 
-our $VERSION = '0.016002';
+our $VERSION = '0.017';
 
 use experimental 'signatures';
 
