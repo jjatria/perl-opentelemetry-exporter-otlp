5d59a6e8d62c47521a9b4ef739da47b47838f78f
José Joaquín Atria <jjatria@cpan.org>
2024-11-24 01:17:25 +0000

    Bump version

--- lib/OpenTelemetry/Exporter/OTLP.pm
+++ lib/OpenTelemetry/Exporter/OTLP.pm
@@ -3,7 +3,7 @@ use Object::Pad ':experimental(init_expr)';
 
 package OpenTelemetry::Exporter::OTLP;
 
-our $VERSION = '0.018';
+our $VERSION = '0.019';
 
 class OpenTelemetry::Exporter::OTLP :does(OpenTelemetry::Exporter) {
     use Feature::Compat::Try;
